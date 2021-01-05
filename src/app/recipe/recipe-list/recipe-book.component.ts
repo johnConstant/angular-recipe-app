391d2e7dedfb242d4bb06a5b32bafcaaad840820
John Constant <john@johns-mbp.home>
2021-01-05 21:53:24 +0000

    Two way binding between recipe detail component and recipe item component

--- src/app/recipe/recipe-list/recipe-book.component.ts
+++ src/app/recipe/recipe-list/recipe-book.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 
 import { Recipe } from "../recipe.model";
 
@@ -8,7 +8,7 @@ import { Recipe } from "../recipe.model";
   styleUrls: ["./recipe-book.component.css"],
 })
 export class RecipeBookComponent implements OnInit {
-  recipes: Recipe[] = [
+  @Output() recipes: Recipe[] = [
     new Recipe(
       "Test Recipe",
       "This is going to be a delicious test recipe",
@@ -20,7 +20,13 @@ export class RecipeBookComponent implements OnInit {
       "https://cdn.pixabay.com/photo/2017/12/10/14/47/piza-3010062_1280.jpg"
     ),
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
